0d499853eec3703f6956fc65136bdf2a9b719a0d
hfsmtk <hafsamottaki@gmail.com>
2025-11-08 21:41:55 +0100

    second commit

---
+++ src/main/java/ma/mundia/bdccmundiaspringmvc/sec/SecurityConfig.java
@@ -0,0 +1,49 @@
+package ma.mundia.bdccmundiaspringmvc.sec;
+
+import jdk.jfr.Enabled;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        PasswordEncoder encoder = passwordEncoder();
+        System.out.println("========");
+        String encodedPWD =encoder.encode("123456");
+        System.out.println(encodedPWD);
+        System.out.println("========");
+
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/public/**").permitAll())
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .build();
+    }
+
+}

7eaf47b3bcf0dea273e8a44497489beef3b9b199
hfsmtk <hafsamottaki@gmail.com>
2025-11-11 22:02:35 +0100

    commit

--- src/main/java/ma/mundia/bdccmundiaspringmvc/sec/SecurityConfig.java
+++ src/main/java/ma/mundia/bdccmundiaspringmvc/sec/SecurityConfig.java
@@ -3,6 +3,7 @@ package ma.mundia.bdccmundiaspringmvc.sec;
 import jdk.jfr.Enabled;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
@@ -38,11 +40,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .formLogin(Customizer.withDefaults())
-                .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
-                .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
-                .authorizeHttpRequests(ar->ar.requestMatchers("/public/**").permitAll())
+                .formLogin(fl ->fl.loginPage("/login").permitAll())
+                .csrf(Customizer.withDefaults())
+                //.authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
+                //.authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/public/**","/webjars/**").permitAll())
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .exceptionHandling(eh->eh.accessDeniedPage("/notAuthorized"))
                 .build();
     }
 
--- src/main/java/ma/mundia/bdccmundiaspringmvc/web/ProductController.java
+++ src/main/java/ma/mundia/bdccmundiaspringmvc/web/ProductController.java
@@ -1,12 +1,15 @@
 package ma.mundia.bdccmundiaspringmvc.web;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import ma.mundia.bdccmundiaspringmvc.entities.Product;
 import ma.mundia.bdccmundiaspringmvc.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +21,7 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
     @GetMapping("/user/index")
+    @PreAuthorize("hasRole('USER')")
     public String index(Model model) {
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList", products);
@@ -27,20 +31,37 @@ public class ProductController {
     public String home() {
         return "redirect:/user/index";
     }
-    @GetMapping("/admin/delete")
+    @PostMapping("/admin/delete")
+    @PreAuthorize("hasRole('ADMIN')")
     public String delete(@RequestParam(name ="id") Long id) {
         productRepository.deleteById(id);
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
     @GetMapping("/admin/newProduct")
+    @PreAuthorize("hasRole('ADMIN')")
     public  String newProduct(Model model) {
         model.addAttribute("product", new Product());
         return "new-product";
     }
     @PostMapping("/admin/saveProduct")
+    @PreAuthorize("hasRole('ADMIN')")
     public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) return "new-product";
         productRepository.save(product);
-        return "redirect:/newProduct";
+        return "redirect:/admin/newProduct";
+    }
+    @GetMapping("/notAuthorized")
+    public  String notAuthorized(){
+        return "notAuthorized";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "login";
     }
 }
